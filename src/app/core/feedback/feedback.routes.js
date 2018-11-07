195c57cf8faa27b13f406d9ac2a9d67c1c216753
Brian Ghigiarelli <brianghig@gmail.com>
2018-11-07 12:15:02 -0500

    Adding Swagger API documentation for Feedback submission endpoint

--- src/app/core/feedback/feedback.routes.js
+++ src/app/core/feedback/feedback.routes.js
@@ -8,6 +8,64 @@ const express = require('express'),
 
 let router = express.Router();
 
+/**
+ * @swagger
+ * definitions:
+ *   Feedback:
+ *     type: object
+ *     properties:
+ *       body:
+ *          type: string
+ *       url:
+ *          type: string
+ *       type:
+ *          type: string
+ *     example:
+ *       body: 'This is a great tool! Thanks for building it.'
+ *       url: 'http://localhost:3000/#/path/to/page'
+ *       type: 'Bug'
+ */
+
+ /**
+ * @swagger
+ * /feedback:
+ *   post:
+ *     produces: application/json
+ *     tags: [Feedback]
+ *     description: >
+ *       Echoes the feedback submitted, including the appended timestamp and user ID
+ *     parameters:
+ *       - name: Feedback
+ *         description: >
+ *             The Feedback that is submitted by the user from some part
+ *             of the application
+ *         in: body
+ *         schema:
+ *           $ref: '#/definitions/Feedback'
+ *     responses:
+ *       '200':
+ *         description: Feedback was submitted successfully
+ *         content:
+ *           application/json:
+ *         schema:
+ *           $ref: '#/definitions/Feedback'
+ *       '401':
+ *         description: Anonymous user attempted to submit feedback
+ *         content:
+ *           application/json:
+ *         type: object
+ *         properties:
+ *           status:
+ *             type: number
+ *           type:
+ *             type: string
+ *           message:
+ *             type: string
+ *         example:
+ *           status: 401
+ *           type: 'no-login'
+ *           message: 'User is not logged in'
+ */
 router.route('/feedback')
 	.post(user.hasAccess, feedback.submitFeedback);
 
