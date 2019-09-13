5c05fa735b71f3b0e23b03364c5bb500efde3b59
Brian Ghigiarelli <brianghig@gmail.com>
2019-09-13 15:11:15 -0400

    Adding Gitlab configuration with proper test environment config (#33)
    * Adding Gitlab configuration with proper test environment config. Fixing Mongoose tests to fail if the DB connection cannot initialize
    
    * update gitlab config to track coverage; remove duplicate html reporter

---
+++ config/env/testgitlab.js
@@ -0,0 +1,43 @@
+'use strict';
+
+module.exports = {
+
+	// Running in test mode
+	//mode: 'test',
+
+	// For the mongoose spec
+	test: {
+		mongoHost: 'mongo'
+	},
+
+	// Use a test db so we don't modify the real DB
+	db: {
+		admin: 'mongodb://mongo/node-rest-starter-test'
+	},
+
+	// Run tests on something other than default port
+	port: 9001,
+
+	// Configuration for outgoing mail server / service
+	mailer: {
+		from: process.env.MAILER_FROM || 'USERNAME@GMAIL.COM',
+		provider: './src/app/core/email/providers/log-email.provider.js',
+		options: {}
+	},
+
+	/**
+	 * Logging Settings
+	 */
+
+	// Disable loggers for tests
+	logger: {
+		application: [
+			{
+				stream: process.stdout,
+				level: 'error'
+			}
+		],
+		audit: []
+	}
+
+};
