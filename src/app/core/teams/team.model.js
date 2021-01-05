dcc2a6e23fdce5d7d606d54bc4cdbb71b7ddc92a
John Rassa <jrassa@asymmetrik.com>
2021-01-05 09:58:40 -0500

    remove unnecessary async

--- src/app/core/teams/team.model.js
+++ src/app/core/teams/team.model.js
@@ -151,7 +151,7 @@ mongoose.model('Team', TeamSchema, 'teams');
 
 // Team Copy of a User ( has team roles for the team )
 const userAuditCopy = UserModel.auditCopy;
-UserSchema.statics.auditCopy = async (user = {}) => {
+UserSchema.statics.auditCopy = (user = {}) => {
 	const toReturn = userAuditCopy(user);
 
 	if (user.constructor.name === 'model') {
