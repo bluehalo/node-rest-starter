bluehalo/node-rest-starter

commit e18de67a8e72c7937145901a2954cadcd60404ab

build: Add optional typescript compile check (#111)
* build: Add optional typescript compile check

The typescript compiler can be run against javascript code and check for various potential issues based on its ability to infer typings.  This can also be supplemented with using JSDoc comments to specify types beyond what can be inferred by the typescript compiler.

Added npm script to execute compiler.  Eventually we may want to run as part of build/ci, but for now leaving it as optional to testing purposes.

* restore schema.service in dependencies.js for backward compatibility

src/lib/socket.io.js
@@ -60,14 +60,15 @@ module.exports = (app, db) => {
 				socket.request.session = session;
 
 				// Use Passport to populate the user details
+				// @ts-ignore
 				passport.initialize()(socket.request, {}, () => {
 					passport.session()(socket.request, {}, () => {
 						if (socket.request.user) {
 							logger.debug('SocketIO: New authenticated user: %s', socket.request.user.username);
-							return next(null, true);
+							return next(null);
 						}
 						logger.info('SocketIO: Unauthenticated user attempting to connect.');
-						return next(new Error('User is not authenticated'), false);
+						return next(new Error('User is not authenticated'));
 					});
 				});
 			});
@@ -77,7 +78,7 @@ module.exports = (app, db) => {
 	// Add an event listener to the 'connection' event
 	io.on('connection', onConnect);
 
-	return server;
+	return app;
 };
 
 /*

.eslintrc
@@ -10,6 +10,10 @@
 		"sourceType": "module"
 	},
 
+  	"plugins": [
+	  	"jsdoc"
+	],
+
 	"extends": "eslint:recommended",
 	"rules": {
 		"arrow-parens": ["error", "always"],
@@ -42,7 +46,8 @@
 		"prefer-template": ["error"],
 		"template-curly-spacing": ["error"],
 		"rest-spread-spacing": ["error"],
-		"prefer-arrow-callback": ["error"]
+		"prefer-arrow-callback": ["error"],
+	  	"jsdoc/no-undefined-types": 1
 
 		//		"arrow-body-style": ["warn", "as-needed"],
 		//		"handle-callback-err": ["warn"],
@@ -51,6 +56,7 @@
 		//		"no-throw-literal": ["warn"],
 		//		"guard-for-in": ["warn"],
 		//		"no-shadow": ["warn"]
+
 	},
 
 	"globals": {

config/env/production.js
@@ -1,7 +1,5 @@
 'use strict';
 
-const logstash = require('bunyan-logstash');
-
 module.exports = {
 
 	/**
@@ -47,16 +45,16 @@ module.exports = {
 				path: '/var/log/mean2/application.log',
 				period: '1d',
 				count: 1
-			},
-			// Logstash logger
-			{
-				type: 'raw',
-				level: 'info',
-				stream: logstash.createStream({
-					host: 'localhost',
-					port: 4561
-				})
 			}
+			// Logstash logger
+			// {
+			// 	type: 'raw',
+			// 	level: 'info',
+			// 	stream: logstash.createStream({
+			// 		host: 'localhost',
+			// 		port: 4561
+			// 	})
+			// }
 		],
 		audit: [
 			// Console logger (audit logger must be 'info' level)

src/app/common/csv-stream.service.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const
+	streams = require('stream'),
 	through2 = require('through2'),
 	stringify = require('csv-stringify'),
 	jsonpath = require('JSONPath'),
@@ -14,14 +15,13 @@ const
  * Creates a stream that accepts objects as input and returns a serialized CSV file.
  * The objects in the stream can come directly from Mongo.
  *
- * @param {Array{Object}} columns An array defining the columns to output in the CSV, in order.
+ * @param {Array.<Object>} columns An array defining the columns to output in the CSV, in order.
  *   Each object must contain:
  *     - key: A JSONPath selector to get the value for this column from the object
  *     - title: The title to use in the CSV header for the column
  *     - callback: Optionally, a function to do further processing of the value.  It must accept a value
  *         and return a value.
- *
- * @returns {Stream} A stream that can be piped to the HTTP response.
+ * @returns {streams.Transform} A stream that can be piped to the HTTP response.
  */
 module.exports = (columns) => {
 

src/app/common/event/event-emitter.service.js
@@ -6,7 +6,7 @@ const EventEmitter = require('events');
  * external messaging system (e.g, RabbitMQ, Kafka, etc.)
  * Only use this EventEmitter in single-instance installations or for dev purposes
  */
-const eventEmitter = new EventEmitter();
+const eventEmitter = new EventEmitter.EventEmitter();
 
 function getEventEmitter() {
 	return eventEmitter;

src/app/common/mongoose/getter.plugin.js
@@ -1,0 +1,13 @@
+/**
+ * Mongoose Getter plugin
+ *
+ * configures schema to include getters in `toObject` and `toJSON`
+ *
+ * @param schema
+ */
+function getterPlugin(schema) {
+	schema.set('toObject', { getters: true });
+	schema.set('toJSON', { getters: true });
+}
+
+module.exports = getterPlugin;

src/app/common/sockets/base-socket.provider.js
@@ -86,7 +86,7 @@ BaseSocket.prototype.getRequest = function() {
  * @param {Function} next A callback for the async handler.  It will be called with an error if the middleware
  *   callback function passes any message to the UI.
  *
- * @returns {{status: status, send: send}}
+ * @returns {{status: Function, send: Function, json: Function}}
  */
 BaseSocket.prototype.getResponse = function(next) {
 	function send(data) {
@@ -118,8 +118,8 @@ BaseSocket.prototype.getResponse = function(next) {
  * Applies a set of callbacks in series.  Each function should accept a request and response object and
  * a callback function, in the same format as the Express.js middleware.
  *
- * @param {Array{Function}} callbacks An array of middleware callbacks to execute.
- * @param {Function=} done Optionally, a function that will be called when all middleware has processed, either
+ * @param {Array.<Function>} callbacks - An array of middleware callbacks to execute.
+ * @param {Function} [done] - Optionally, a function that will be called when all middleware has processed, either
  *   with an error or without.
  *
  * @returns {Promise} A promise that will be resolved when all the middleware has run.  You can either

src/app/common/sockets/event-socket.provider.js
@@ -2,7 +2,6 @@
 
 const
 	_ = require('lodash'),
-	nodeUtil = require('util'),
 
 	deps = require('../../../dependencies'),
 	config = deps.config,
@@ -15,162 +14,158 @@ const
 // If this is not null, ignore any messages that are older than this number of seconds.
 const ignoreOlderThan = config.socketio.ignoreOlderThan;
 
-function EventSocket(socketConfig, ...args) {
-	this._emitRateMs = socketConfig.emitRateMs < 0 ? 0 : (+socketConfig.emitRateMs || 0);
-	BaseSocket.apply(this, [socketConfig, ...args]);
-}
-
-nodeUtil.inherits(
-	EventSocket,
-	BaseSocket);
-
-EventSocket.prototype.name = 'EventSocket';
-
-/**
- * Handle socket disconnects by unsubscribing from events.
- */
-EventSocket.prototype.disconnect = function() {
-	logger.info('%s: Disconnected from client.', this.name);
-	this.unsubscribe(null);
-};
-
-/**
- * Handle socket errors by unsubscribing from events.
- */
-EventSocket.prototype.error = function(err) {
-	logger.error(err, '%s: Client connection error', this.name);
-	this.unsubscribe(null);
-};
-
-/**
- * Returns the name of the socket event that will be transmitted to the client.
- * This should be overridden by each implementing class.
- *
- * @returns {string} The event name to transmit through the socket for each payload.
- */
-EventSocket.prototype.getEmitType = function() {
-	return this._emitType || 'payload';
-};
-
-/**
- * Extracts a timestamp from the payload, which can be used for filtering messages.
- *
- * @param {Object} json The payload, parsed as JSON.
- * @returns {Number} Returns the timestamp of the payload as a Long.
- */
-EventSocket.prototype.getMessageTime = function(json) {
-	// Default to extracting time from json
-	if (null != json) {
-		const time = json.time;
-		logger.debug('%s: Extracted message time of %d', this.name, time);
-		return time;
+class EventSocket extends BaseSocket {
+	constructor(socketConfig) {
+		super(socketConfig);
+		this._emitType = null;
+		this._emitRateMs = socketConfig.emitRateMs < 0 ? 0 : (+socketConfig.emitRateMs || 0);
 	}
 
-	if (logger.debug()) { // is debug enabled?
-		logger.debug('%s: Unknown time for message: %s', this.name, JSON.stringify(json));
+	/**
+	 * Handle socket disconnects by unsubscribing from events.
+	 */
+	disconnect() {
+		logger.info('%s: Disconnected from client.', this.name);
+		this.unsubscribe(null);
 	}
 
-	return null;
-};
+	/**
+	 * Handle socket errors by unsubscribing from events.
+	 */
+	error(err) {
+		logger.error(err, '%s: Client connection error', this.name);
+		this.unsubscribe(null);
+	}
 
-/**
- * Filters a payload to determine whether it should be transmitted. This should be overridden by the
- * implementing class. It does not need to filter by date, as this is done automatically for all payloads.
- *
- * @param {Object} json The payload, parsed as JSON.
- * @return {boolean} False if the payload should be sent to the client, true if it should be ignored.
- */
-EventSocket.prototype.ignorePayload = function(json) {
-	// Ignore any payloads that are too old.
-	if (null != ignoreOlderThan) {
-		const now = Date.now();
-		const messageTime = this.getMessageTime(json);
-		if (null != messageTime) {
-			if (messageTime + (ignoreOlderThan * 1000) < now) {
-				logger.debug('%s: Message is too old: %d is more than %d seconds older than %d', this.name, messageTime, ignoreOlderThan, now);
-				return true;
-			}
+	/**
+	 * Returns the name of the socket event that will be transmitted to the client.
+	 * This should be overridden by each implementing class.
+	 *
+	 * @returns {string} The event name to transmit through the socket for each payload.
+	 */
+	getEmitType() {
+		return this._emitType || 'payload';
+	}
+
+	/**
+	 * Extracts a timestamp from the payload, which can be used for filtering messages.
+	 *
+	 * @param {Object} json The payload, parsed as JSON.
+	 * @returns {Number} Returns the timestamp of the payload as a Long.
+	 */
+	getMessageTime(json) {
+		// Default to extracting time from json
+		if (null != json) {
+			const time = json.time;
+			logger.debug('%s: Extracted message time of %d', this.name, time);
+			return time;
 		}
-	}
-	return false;
-};
 
-/**
- * Allows child sockets to customize the way messages are emitted, for instance to provide more advanced throttling.
- * Default implementation emits to the socket as usual.
- *
- * @param {string} emitType The emit type
- * @param {Object} msg The message to emit
- */
-EventSocket.prototype.emitMessage = function(emitType, msg) {
-	this.getSocket().emit(emitType, msg);
-};
+		if (logger.debug()) { // is debug enabled?
+			logger.debug('%s: Unknown time for message: %s', this.name, JSON.stringify(json));
+		}
 
-/**
- * Subscribe to an event.
- *
- * @return null if eventName is not set, true if successful
- */
-EventSocket.prototype.subscribe = function(eventName) {
-	// Ignore bad input data
-	if (null == eventName) {
 		return null;
 	}
 
-	// Simple throttling is done here, if enabled
-
-	if (this._emitRateMs > 0) {
-		this.emitterFunc = _.throttle(this.socketPayloadHandler, this._emitRateMs).bind(this, eventName);
-	} else {
-		this.emitterFunc = this.socketPayloadHandler.bind(this, eventName);
-	}
-	eventEmitter.getEventEmitter().on(eventName, this.emitterFunc);
-};
-
-/**
- * Unsubscribe from a topic.  If no topic is specified, unsubscribes from all topics consumed by this socket.
- *
- * @param {string} topic The topic to unsubscribe from (optional).
- */
-EventSocket.prototype.unsubscribe = function(eventName) {
-	if (typeof this.emitterFunc === 'function') {
-		eventEmitter.getEventEmitter().removeListener(eventName, this.emitterFunc);
-	}
-};
-
-EventSocket.prototype.socketPayloadHandler = function(eventName, message) {
-	// Gracefully handle empty messages by ignoring and logging
-	if (null == message) {
-		logger.warn('%s: Ignoring empty message %s', this.name, message);
-		return;
-	}
-
-	const self = this;
-	logger.debug('%s: Received Event Message for event %s', this.name, eventName);
-	try {
-		// Unwrap the payload
-		if (null != message) {
-
-			// Ignore any payloads that don't pass the filter check.
-			if (self.ignorePayload(message)) {
-				return;
+	/**
+	 * Filters a payload to determine whether it should be transmitted. This should be overridden by the
+	 * implementing class. It does not need to filter by date, as this is done automatically for all payloads.
+	 *
+	 * @param {Object} json The payload, parsed as JSON.
+	 * @return {boolean} False if the payload should be sent to the client, true if it should be ignored.
+	 */
+	ignorePayload(json) {
+		// Ignore any payloads that are too old.
+		if (null != ignoreOlderThan) {
+			const now = Date.now();
+			const messageTime = this.getMessageTime(json);
+			if (null != messageTime) {
+				if (messageTime + (ignoreOlderThan * 1000) < now) {
+					logger.debug('%s: Message is too old: %d is more than %d seconds older than %d', this.name, messageTime, ignoreOlderThan, now);
+					return true;
+				}
 			}
+		}
+		return false;
+	}
 
-			// The message can be either an object or a promise for an object
-			Promise.all([message]).then(([msg]) => {
-				if (null != msg) {
-					self.emitMessage(self.getEmitType(), msg);
-				}
-			}).catch(function(err) {
-				if (logger.debug()) {
-					logger.debug('Ignoring payload for user %s: %s', this.getUserId(), err);
-				}
-			});
+	/**
+	 * Allows child sockets to customize the way messages are emitted, for instance to provide more advanced throttling.
+	 * Default implementation emits to the socket as usual.
+	 *
+	 * @param {string} emitType The emit type
+	 * @param {Object} msg The message to emit
+	 */
+	emitMessage(emitType, msg) {
+		this.getSocket().emit(emitType, msg);
+	}
+
+	/**
+	 * Subscribe to an event.
+	 *
+	 * @return null if eventName is not set, true if successful
+	 */
+	subscribe(eventName) {
+		// Ignore bad input data
+		if (null == eventName) {
+			return null;
+		}
+
+		// Simple throttling is done here, if enabled
+
+		if (this._emitRateMs > 0) {
+			this.emitterFunc = _.throttle(this.socketPayloadHandler, this._emitRateMs).bind(this, eventName);
+		} else {
+			this.emitterFunc = this.socketPayloadHandler.bind(this, eventName);
+		}
+		eventEmitter.getEventEmitter().on(eventName, this.emitterFunc);
+	}
+
+	/**
+	 * Unsubscribe from a topic.  If no topic is specified, unsubscribes from all topics consumed by this socket.
+	 *
+	 * @param {string} eventName The topic to unsubscribe from (optional).
+	 */
+	unsubscribe(eventName) {
+		if (typeof this.emitterFunc === 'function') {
+			eventEmitter.getEventEmitter().removeListener(eventName, this.emitterFunc);
 		}
 	}
-	catch (e) {
-		logger.error({err: e, msg: message.value }, '%s: Error parsing payload body.', this.name);
+
+	socketPayloadHandler(eventName, message) {
+		// Gracefully handle empty messages by ignoring and logging
+		if (null == message) {
+			logger.warn('%s: Ignoring empty message %s', this.name, message);
+			return;
+		}
+
+		const self = this;
+		logger.debug('%s: Received Event Message for event %s', this.name, eventName);
+		try {
+			// Unwrap the payload
+			if (null != message) {
+
+				// Ignore any payloads that don't pass the filter check.
+				if (self.ignorePayload(message)) {
+					return;
+				}
+
+				// The message can be either an object or a promise for an object
+				Promise.all([message]).then(([msg]) => {
+					if (null != msg) {
+						self.emitMessage(self.getEmitType(), msg);
+					}
+				}).catch(function (err) {
+					if (logger.debug()) {
+						logger.debug('Ignoring payload for user %s: %s', this.getUserId(), err);
+					}
+				});
+			}
+		} catch (e) {
+			logger.error({err: e, msg: message.value}, '%s: Error parsing payload body.', this.name);
+		}
 	}
-};
+}
 
 module.exports = EventSocket;

commit 008896ad496d0369cfa8e5a7cbb26a80bbfc6beb

fix: Fix socket.io server not starting properly
Fixes issue introduced in #111

src/lib/express.js
@@ -269,6 +269,10 @@ module.exports.init = function (db) {
 
 	// Initialize express app
 	logger.info('Initializing Express');
+
+	/**
+	 * @type {*|express}
+	 */
 	let app = express();
 
 	// Initialize local variables

src/lib/socket.io.js
@@ -78,7 +78,7 @@ module.exports = (app, db) => {
 	// Add an event listener to the 'connection' event
 	io.on('connection', onConnect);
 
-	return app;
+	return server;
 };
 
 /*