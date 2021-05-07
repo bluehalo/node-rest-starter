99349caa3ea8ec09dc6868ba9faf3142ede0e432
John Rassa <jrassa@asymmetrik.com>
2021-05-07 08:59:34 -0400

    fix(audit): Audit schema/index fixes
    * Updated Audit schema to correctly represent what is being store.  `audit` property was being created with a SchemaType of Mixed.  This caused mongoose to skip all validation as Mixed can be anything.  `audit.object` property is now explicitly set as Mixed since it can be an Object or string.
    * Updated Audit indexes to match how the audit collection is queried.  Removed `audit.object` index.  Replaced `audit.actor` index with `audit.actor._id` and `audit.actor.name` indexes.
    * Added type definitions

--- src/app/core/audit/audit.model.js
+++ src/app/core/audit/audit.model.js
@@ -9,6 +9,9 @@ const mongoose = require('mongoose'),
 
 /**
  * Schema Declaration
+ *
+ * @typed {mongoose.Schema<import('./types').AuditDocument, import('./types').AuditModel>}
+ * @type {mongoose.Schema}
  */
 const AuditSchema = new mongoose.Schema({
 	created: {
@@ -18,18 +21,13 @@ const AuditSchema = new mongoose.Schema({
 	},
 	message: { type: String },
 	audit: {
-		type: {
-			auditType: { type: String },
-			action: { type: String },
-			actor: { type: String },
-			interfaceUrl: { type: String },
-			object: { type: String },
-			userSpec: {
-				type: {
-					browser: { type: String },
-					os: { type: String }
-				}
-			}
+		auditType: { type: String },
+		action: { type: String },
+		actor: { type: Object },
+		object: { type: mongoose.Schema.Types.Mixed },
+		userSpec: {
+			browser: { type: String },
+			os: { type: String }
 		}
 	}
 });
@@ -37,7 +35,7 @@ const AuditSchema = new mongoose.Schema({
 AuditSchema.plugin(getterPlugin);
 AuditSchema.plugin(paginatePlugin);
 AuditSchema.plugin(containsSearchPlugin, {
-	fields: ['message', 'audit.auditType', 'audit.action', 'audit.object']
+	fields: ['message', 'audit.auditType', 'audit.action']
 });
 
 /**
@@ -53,11 +51,11 @@ AuditSchema.index({ 'audit.auditType': 1 });
 // Audit Action index
 AuditSchema.index({ 'audit.action': 1 });
 
-// User index
-AuditSchema.index({ 'audit.actor': 1 });
+// actor._id index
+AuditSchema.index({ 'audit.actor._id': 1 });
 
-// Audit object index
-AuditSchema.index({ 'audit.object': 1 });
+// actor.name index
+AuditSchema.index({ 'audit.actor.name': 1 });
 
 // Audit message index
 AuditSchema.index({ message: 1 });
@@ -73,4 +71,10 @@ AuditSchema.index({ message: 1 });
 /**
  * Register the Schema with Mongoose
  */
-mongoose.model('Audit', AuditSchema, 'audit');
+/**
+ *
+ * @type {import('./types').AuditModel}
+ */
+const Audit = mongoose.model('Audit', AuditSchema, 'audit');
+
+module.exports = Audit;
---
+++ src/app/core/audit/types.d.ts
@@ -0,0 +1,20 @@
+import { Document, Model } from 'mongoose';
+
+interface IAudit extends Document {
+	created: Date;
+	message: string;
+	audit: {
+		auditType: string;
+		action: string;
+		actor: Object;
+		object: string | Object;
+		userSpec: {
+			browser: string;
+			os: string;
+		};
+	};
+}
+
+export interface AuditDocument extends IAudit {}
+
+export interface AuditModel extends Model<AuditDocument> {}
