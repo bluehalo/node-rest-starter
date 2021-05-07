3804b811b2cf59ed86e49f2c4cf04e3c1a209da5
John Rassa <jrassa@asymmetrik.com>
2021-05-07 08:06:32 -0400

    refactor: Update mongoose plugins for improved type safety and usability
    * Updated search plugins to use `where` vs. `find` for adding query clause.  This allows them to be used with more query types (i.e. find, findOne, count, ...)
    * Updated plugin type definitions to specify correct return types
    * Updated paginate function definition to properly mark optional parameters

--- src/app/common/mongoose/types.d.ts
+++ src/app/common/mongoose/types.d.ts
@@ -1,16 +1,26 @@
+import { Document } from 'mongoose';
+
+export interface PagingResults<DocType extends Document = Document> {
+	pageNumber: number;
+	pageSize: number;
+	totalPages: number;
+	totalSize: number;
+	elements: DocType[];
+}
+
 export interface TextSearchPlugin {
-	textSearch(search: string);
+	textSearch(search: string): this;
 }
 
 export interface ContainsSearchPlugin {
-	containsSearch(search: string, fields?: string[]);
+	containsSearch(search: string, fields?: string[]): this;
 }
 
-export interface PaginatePlugin {
+export interface PaginatePlugin<DocType extends Document> {
 	paginate(
 		pageSize: number,
 		pageNumber: number,
-		runCount: boolean,
-		countTimeout: number
-	);
+		runCount?: boolean,
+		countTimeout?: number
+	): Promise<PagingResults<DocType>>;
 }
