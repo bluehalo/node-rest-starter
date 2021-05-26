e82de19b1f62f6add20295e58a24c52774ed1085
John Rassa <jrassa@asymmetrik.com>
2021-05-26 08:28:05 -0400

    fix(audit): Audit schema/index fixes (#129)
    * fix(audit): Audit schema/index fixes
    
    * Updated Audit schema to correctly represent what is being store.  `audit` property was being created with a SchemaType of Mixed.  This caused mongoose to skip all validation as Mixed can be anything.  `audit.object` property is now explicitly set as Mixed since it can be an Object or string.
    * Updated Audit indexes to match how the audit collection is queried.  Removed `audit.object` index.  Replaced `audit.actor` index with `audit.actor._id` and `audit.actor.name` indexes.
    * Added type definitions
    
    * switch back method of accessing AuditModel
    
    * resolve github action warning

--- src/app/core/audit/audit.service.js
+++ src/app/core/audit/audit.service.js
@@ -5,56 +5,70 @@ const deps = require('../../../dependencies'),
 	logger = deps.logger,
 	auditLogger = deps.auditLogger;
 
-// Creates an audit entry persisted to Mongo and the bunyan logger
-module.exports.audit = function (
+/**
+ * Creates an audit entry persisted to Mongo and the bunyan logger
+ *
+ * @param {string} message
+ * @param {string} eventType
+ * @param {string} eventAction
+ * @param {* | Promise<*>} eventActor
+ * @param {*} eventObject
+ * @param {*} [eventMetadata]
+ * @param {boolean} [stringifyEventObject]
+ * @returns {Promise<import('./types').AuditDocument>}
+ */
+module.exports.audit = async (
 	message,
 	eventType,
 	eventAction,
 	eventActor,
 	eventObject,
-	eventMetadata,
-	stringifyEventObject
-) {
+	eventMetadata = null,
+	stringifyEventObject = false
+) => {
+	/**
+	 * @type {import('./types').AuditModel}
+	 */
 	const Audit = dbs.admin.model('Audit');
 	const utilService = deps.utilService;
 
-	return Promise.resolve(eventActor).then((actor) => {
-		// Extract additional metadata to audit
-		const userAgentObj = utilService.getUserAgentFromHeader(eventMetadata);
+	const actor = await Promise.resolve(eventActor);
 
-		// Send to Mongo
-		const newAudit = new Audit({
-			created: Date.now(),
-			message: message,
-			audit: {
-				auditType: eventType,
-				action: eventAction,
-				actor: actor,
-				userSpec: userAgentObj
-			}
-		});
+	// Extract additional metadata to audit
+	const userAgentObj = utilService.getUserAgentFromHeader(eventMetadata);
 
-		newAudit.audit.object = stringifyEventObject
-			? JSON.stringify(eventObject)
-			: eventObject;
+	// Send to Mongo
+	const newAudit = new Audit({
+		created: Date.now(),
+		message: message,
+		audit: {
+			auditType: eventType,
+			action: eventAction,
+			actor: actor,
+			userSpec: userAgentObj
+		}
+	});
 
-		// Send to bunyan logger for logfile persistence
-		auditLogger.audit(
-			message,
-			eventType,
-			eventAction,
-			actor,
-			eventObject,
-			userAgentObj
+	newAudit.audit.object = stringifyEventObject
+		? JSON.stringify(eventObject)
+		: eventObject;
+
+	// Send to bunyan logger for logfile persistence
+	auditLogger.audit(
+		message,
+		eventType,
+		eventAction,
+		actor,
+		eventObject,
+		userAgentObj
+	);
+
+	return newAudit.save().catch((err) => {
+		// Log and continue the error
+		logger.error(
+			{ err: err, audit: newAudit },
+			'Error trying to persist audit record to storage.'
 		);
-
-		return newAudit.save().catch((err) => {
-			// Log and continue the error
-			logger.error(
-				{ err: err, audit: newAudit },
-				'Error trying to persist audit record to storage.'
-			);
-			return Promise.reject(err);
-		});
+		return Promise.reject(err);
 	});
 };
