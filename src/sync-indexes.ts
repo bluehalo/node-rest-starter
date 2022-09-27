6b9b1436c76e01919be077423962020069e78602
John Rassa <jrassa@asymmetrik.com>
2022-09-27 16:43:50 -0400

    feat: add utility script to sync mongoose indexes

---
+++ src/sync-indexes.ts
@@ -0,0 +1,33 @@
+import { models } from 'mongoose';
+
+import { logger } from './lib/bunyan';
+import mongoose from './lib/mongoose';
+
+const syncIndex = async () => {
+	logger.info('Started index sync...');
+
+	await mongoose.connect();
+
+	try {
+		logger.info('Mongoose connected, proceeding with index sync');
+
+		for (const model of Object.values(models)) {
+			logger.info(`syncing indexes for model: ${model.modelName} `);
+			// eslint-disable-next-line no-await-in-loop
+			await model.syncIndexes();
+		}
+	} catch (err) {
+		logger.fatal('failed.');
+		return Promise.reject(err);
+	}
+};
+
+syncIndex()
+	.then(() => {
+		logger.info('Index sync complete');
+		process.exit(0);
+	})
+	.catch((error) => {
+		logger.fatal(error, 'Startup initialization failed.');
+		process.exit(1);
+	});
