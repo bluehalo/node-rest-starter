00205040168369e80b88a45a0eb569d2fdd95d28
John Rassa <john.rassa@bluehalo.com>
2024-02-05 10:29:37 -0500

    feat: rework asset globbing
    * Assets now defined in config vs. separate file
    * Updated glob library to latest version.  Newer version has built in support for globbing multiple patterns at once.
    * Removed multi-glob pattern processing code.
    * globbing now handled on demand by the code that needs it.

--- src/config.js
+++ src/config.js
@@ -7,50 +7,6 @@ const _ = require('lodash'),
 	path = require('path');
 
 /**
- * Get files by glob patterns
- * @param {string | string[]} globPatterns
- * @param {string | string[]} [excludes]
- */
-function getGlobbedPaths(globPatterns, excludes) {
-	// URL paths regex
-	const urlRegex = new RegExp('^(?:[a-z]+:)?//', 'i');
-
-	// The output array
-	let output = [];
-
-	// If glob pattern is array so we use each pattern in a recursive way, otherwise we use glob
-	if (_.isArray(globPatterns)) {
-		globPatterns.forEach((globPattern) => {
-			output = _.union(output, getGlobbedPaths(globPattern, excludes));
-		});
-	} else if (_.isString(globPatterns)) {
-		if (urlRegex.test(globPatterns)) {
-			output.push(globPatterns);
-		} else {
-			let files = glob.sync(globPatterns);
-
-			if (excludes) {
-				files = files.map((file) => {
-					if (_.isArray(excludes)) {
-						for (const i in excludes) {
-							file = file.replace(excludes[i], '');
-						}
-					} else {
-						file = file.replace(excludes, '');
-					}
-
-					return file;
-				});
-			}
-
-			output = _.union(output, files);
-		}
-	}
-
-	return output;
-}
-
-/**
  * Validate NODE_ENV existence
  */
 function validateEnvironmentVariable() {
@@ -90,35 +46,6 @@ function validateConfiguration(config) {
 	console.log(chalkFn(msg));
 }
 
-/**
- * Initialize global configuration files
- */
-function initGlobalConfigFiles(config, assets) {
-	// Appending files
-	config.files = {};
-
-	// Setting Globbed model files
-	config.files.models = getGlobbedPaths(assets.models);
-
-	// Setting Globbed route files
-	config.files.routes = getGlobbedPaths(assets.routes);
-
-	// Setting Globbed config files
-	config.files.configs = getGlobbedPaths(assets.config);
-
-	// Setting Globbed socket files
-	config.files.sockets = getGlobbedPaths(assets.sockets);
-
-	// Setting Globbed test files
-	config.files.tests = getGlobbedPaths(assets.tests);
-
-	// Setting Globbed e2e test files
-	config.files.e2e = getGlobbedPaths(assets.e2e);
-
-	// Setting Globbed doc files
-	config.files.docs = getGlobbedPaths(assets.docs);
-}
-
 function initDerivedConfig(config) {
 	if (
 		config.app &&
@@ -156,20 +83,6 @@ function initGlobalConfig() {
 
 	// Validate Critical configuration settings
 	validateConfiguration(config);
-
-	// Get the assets
-	const assets = require(path.posix.join(process.cwd(), 'config/assets'));
-
-	// Initialize global globbed files
-	initGlobalConfigFiles(config, assets);
-
-	// Store the original assets in the config
-	config.assets = assets;
-
-	// Expose configuration utilities
-	config.utils = {
-		getGlobbedPaths: getGlobbedPaths
-	};
 
 	// Initialize derived config values
 	initDerivedConfig(config);
--- src/lib/passport.ts
+++ src/lib/passport.ts
@@ -1,9 +1,9 @@
 import path from 'path';
 
+import { globSync } from 'glob';
 import passport from 'passport';
 
 import { User } from '../app/core/user/user.model';
-import { config } from '../dependencies';
 
 export const init = async () => {
 	// Serialize sessions
@@ -25,16 +25,14 @@ export const init = async () => {
 
 	// Initialize strategies
 	await Promise.all(
-		config.utils
-			.getGlobbedPaths([
-				'./src/lib/strategies/**/*.js',
-				'./src/lib/strategies/**/*.ts'
-			])
-			.map(async (strategyPath) => {
-				const { default: strategy } = await import(
-					path.posix.resolve(strategyPath)
-				);
-				passport.use(strategy);
-			})
+		globSync([
+			'./src/lib/strategies/**/*.js',
+			'./src/lib/strategies/**/*.ts'
+		]).map(async (strategyPath) => {
+			const { default: strategy } = await import(
+				path.posix.resolve(strategyPath)
+			);
+			passport.use(strategy);
+		})
 	);
 };
